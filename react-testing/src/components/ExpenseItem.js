5ee7a74f8ec7d2253b99928ff5470a43a3d7189e
Devin Broussard <devin.ray.broussard@gmail.com>
2022-03-04 12:51:19 -0600

    Added props to the expense item

--- react-testing/src/components/ExpenseItem.js
+++ react-testing/src/components/ExpenseItem.js
@@ -1,19 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 3, 2); 
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
-    return (
-        <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
-            </div>
-        </div>
-    )
+function ExpenseItem({title, amount, date}) {
+  return (
+    <div className="expense-item">
+      <div>{date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
 }
+
 
 export default ExpenseItem;
