ef5fba1230eba4c8c49314f06deec74bd4790122
Devin Broussard <devin.ray.broussard@gmail.com>
2022-03-30 14:23:46 -0500

    Update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 import './Card.css'
 
 //Used as a reusable "wrapper" component to make a block look card-like
-//children: the children html elements inside of the card
+//children: the html elements inside of the card
 function Card({className, children}) {
     //Using the className prop to assign the div any css class
     const classes = 'card ' + className;
-    
+
     return <div className={classes}>{children}</div>
 }
 
