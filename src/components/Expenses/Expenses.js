00b95d73a080915de4ca804dc174b1df87b98803
Devin Broussard <devin.ray.broussard@gmail.com>
2022-04-07 11:55:29 -0500

    Organized component files

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,19 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from './UI/Card'
+import './Expenses.css';
+
+function Expenses({expenses}) {
+  return (
+    <Card className='expenses'>
+      {expenses.map((item) => (
+        <ExpenseItem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
