15b155bc35bfdcd8258a614fe62ceeffcc00eebb
Devin Broussard <devin.ray.broussard@gmail.com>
2022-04-21 11:46:06 -0500

    Working with states for the first time

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,19 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import "./ExpenseItem.css";
 
 //Takes in a title, amount, and date using props
 const ExpenseItem = ({ title, amount, date } /*Deconstructing props*/) => {
+  /*Setting the default state of this component.
+  The "useState" function returns an array that has the state itself, and a function to change the state.
+  When states are changed, it tells react to call this function again.*/
+  const [defaultState, setTitle] = useState(title);
+
+  /*It is a convention for event function to have "handler" at the end*/
   const clickedHandler = () => {
-    console.log('Clicked');
+    console.log('Clicked")')
+    title = 'Updated';
   }
 
   return (
